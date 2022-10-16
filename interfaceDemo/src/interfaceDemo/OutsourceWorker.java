0fa4843e15b1d2ec0432f8f3facd9b23190ca2aa
seymaozbalta <seymaozbalta@gmail.com>
2022-10-16 23:50:15 +0300

    practice about interface

---
+++ interfaceDemo/src/interfaceDemo/OutsourceWorker.java
@@ -0,0 +1,11 @@
+package interfaceDemo;
+
+public class OutsourceWorker implements IWorkable{
+//dışardan gelen çalışanın iş kuralları
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ interfaceDemo/src/interfaceDemo/Robot.java
@@ -0,0 +1,12 @@
+package interfaceDemo;
+
+public class Robot implements IWorkable{
+//örneğin robotlar çalışır,maaş yok o sebeple payable yok,yemek yok ıeatable yok
+	//ancak onlara özel bakım var, yeni bir bakım sınıfı yazılarak implemente edilir
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
